cee4020c4521665348902f55013c70ec2f123aa5
Tom Stockwood <tomstockwood@hotmail.co.uk>
2019-12-13 15:39:33 +0000

    //Add on to the previous commit

--- src/App.js
+++ src/App.js
@@ -67,7 +67,6 @@ class App extends Component {
             
             <MessageList
               messages={this.state.messages}
-              users={this.state.users}
               user={this.state.users[0]}
             >
             </MessageList>
